6629f08b320850625041557fb34f7aac5946e173
aeromatter <9001101+aeromatter@users.noreply.github.com>
2023-11-02 11:33:18 +0000

    Added playlist script
    Downloads music from videos specified in a music.json file

---
+++ playlist.py
@@ -0,0 +1,26 @@
+import json
+from yt_dlp import YoutubeDL
+
+names = []
+urls = []
+full_url = "https://www.youtube.com/watch?v="
+
+with open('music.json') as data_file:
+	for item in json.load(data_file):
+		names.append(item["song_name"])
+		urls.append(full_url + item["song_id"])
+
+songs = zip(names, urls)
+
+for (name, url) in songs:
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'vorbis',
+        }],
+        'outtmpl': f"~/test/{name}.%(ext)s"
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download(url)
